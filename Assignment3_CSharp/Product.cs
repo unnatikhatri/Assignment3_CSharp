f23c2070f9b463c55b3cca1479215a3b7a1d200a
unnatikhatri <59996056+unnatikhatri@users.noreply.github.com>
2020-01-31 16:26:01 +0530

    Add files via upload

---
+++ Assignment3_CSharp/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_CSharp
+{
+    public class Product 
+    {
+        private string name;
+        private float price;
+        private int quantity;
+        private string type;
+
+
+        public Product()
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Quantity = quantity;
+            this.Type = type;
+
+        }
+
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+        public string Type { get; set; }
+
+    }
+}
---
+++ Assignment3_CSharp/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Products = new List<Product>();
+
+            //Adding Elements
+            Products.Add(new Product()
+            {
+                Name = "Lettuce",
+                Price = 10.5f,
+                Quantity = 50,
+                Type = "Leafy green"
+            });
+            Products.Add(new Product()
+            {
+                Name = "Cabbage",
+                Price = 20,
+                Quantity = 100,
+                Type = "Cruciferous"
+            });
+            Products.Add(new Product()
+            {
+                Name = "Pumpkin",
+                Price = 30,
+                Quantity = 30,
+                Type = "Marrow"
+            });
+            Products.Add(new Product()
+            {
+                Name = "Cauliflower",
+                Price = 10,
+                Quantity = 25,
+                Type = "Cruciferous"
+            });
+            Products.Add(new Product()
+            {
+                Name = "Zucchini",
+                Price = 20.5f,
+                Quantity = 50,
+                Type = "Marrow"
+            });
+            Products.Add(new Product()
+            {
+                Name = "Yam",
+                Price = 30,
+                Quantity = 50,
+                Type = "Root"
+            });
+            Products.Add(new Product()
+            {
+                Name = "Spinach",
+                Price = 10,
+                Quantity = 100,
+                Type = "Leafy Green"
+            });
+            Products.Add(new Product()
+            {
+                Name = "Broccoli",
+                Price = 20.2f,
+                Quantity = 75,
+                Type = "Cruciferous"
+            });
+            Products.Add(new Product()
+            {
+                Name = "Garlic",
+                Price = 30,
+                Quantity = 20,
+                Type = "Leafy Green"
+            });
+            Products.Add(new Product()
+            {
+                Name = "Silverbeet",
+                Price = 10,
+                Quantity = 50,
+                Type = "Marrow"
+            });
+
+            //Printing total product numbers
+            Console.WriteLine("Total number of products " + Products.Count());
+
+            //Adding new element 
+            Products.Add(new Product()
+            {
+                Name = "Potato",
+                Price = 10,
+                Quantity = 50,
+                Type = "Root"
+            });
+
+            //Printing list elements
+            Console.WriteLine("\n \n \n List of available products\n");
+            foreach (var p in Products)
+            {
+                Console.WriteLine(p.Name + " : " + p.Price + " : " + p.Quantity + " : " + p.Type);
+            }
+
+            //Printing count after new insertion
+            Console.WriteLine("\n \n Total number of Products now :"+ Products.Count());
+
+
+            //Printing leafy products
+            
+            Console.WriteLine("\n \n \nLeafy Products are :");
+            foreach (var p in Products)
+            {
+                if (p.Type == "Leafy green")
+                    Console.WriteLine(p.Name + " : " + p.Price + " : " + p.Quantity + " : " + p.Type);
+            }
+
+            //Removing Garlic and Printing Total Number of Products Available
+            Products.RemoveAt(9);
+
+            Console.WriteLine("\n \n \nProducts after garlics were sold out :" + Products.Count());
+
+            //User adding 50 cabbages so total no. of cabbages displayed
+            foreach (var p in Products)
+            {
+                if (p.Name == "Cabbage")
+                {
+                    int temp = p.Quantity + 50;
+                    Console.WriteLine("\n \n Number of cabbages :" + temp);
+                }
+            }
+
+            //Calculating amount for 1kg lettuce, 2kg zucchini, 1kg broccoli
+            double amount;
+            amount = ((1 * 10.5) + (2 * 20.5) + (1 * 20.2));
+            amount = Math.Round(amount);
+            Console.WriteLine("\n \n \n Amount : Rs." + amount);
+        }
+    }
+}
